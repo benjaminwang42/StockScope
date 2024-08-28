6f5e2e3ba8b6967ec6c8ebb3dff7fa99e1ee3042
Benjamin Wang <benjaminwang42@gmail.com>
2024-08-27 20:14:00 -0400

    Remove dummy text

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -4,8 +4,8 @@ import "../Styles/Home.css";
 function Home() {
   return (
     <div class="row home">
-      <div class="col-8 col-sm-8 home-display">test</div>
-      <div class="col-4 col-sm-4 home-actions">test</div>
+      <div class="col-8 col-sm-8 home-display"></div>
+      <div class="col-4 col-sm-4 home-actions"></div>
     </div>
   );
 }
